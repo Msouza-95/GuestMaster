860dabdc0313521c112f62124d56859b3073de09
Matheus Souza dos santos <msdsantos96@gmail.com>
2024-06-21 20:14:13 -0300

    fefactor(room): refactoring controllers and routes

--- src/module/room/infra/http/controller/room.controller.ts
+++ src/module/room/infra/http/controller/room.controller.ts
@@ -1,33 +1,79 @@
 import { Request, Response } from "express";
-import CreateRoomUseCase from "module/room/use-cases/create-room-user";
+import ShowRoomUseCase from "module/room/use-cases/show-room";
+import CreateRoomUseCase from "module/room/use-cases/create-room";
+import DeleteRoomUseCase from "module/room/use-cases/delete-room";
+import UpdateRoomUseCase from "module/room/use-cases/update-room";
 
 import { container } from "tsyringe";
 
 
-
-
-
-export class RoomControler{
+export class RoomController{
 
   async create(request: Request, response: Response): Promise<Response> {
 
     // recupera os dados do body da rota
-    const { room_number, room_type, status, hotel_id } = request.body;
+    const { room_number, room_type, hotel_id } = request.body;
 
       // resolve depedencia
     const createRoomUseCase = container.resolve(CreateRoomUseCase)
 
-
-      // envia os dados para serviço de criação
-    const newRoom = await createRoomUseCase.execute({room_number, room_type, status, hotel_id})
+          // envia os dados para serviço de criação
+    const newRoom = await createRoomUseCase.execute({room_number, room_type, hotel_id})
 
     return response.status(201).json({room: newRoom})
   }
   async show(request: Request, response: Response): Promise<Response> {
 
+    const {hotel_id} = request.params;
+
+    const showRoomUseCase = container.resolve(ShowRoomUseCase)
+
+    const rooms = await showRoomUseCase.showExecute(hotel_id)
+
+    return response.status(200).json({rooms: rooms})
+  }
+  async showType(request: Request, response: Response): Promise<Response> {
+
+    const {hotel_id, room_type} = request.params;
+
+    const showRoomUseCase = container.resolve(ShowRoomUseCase)
+
+    const rooms = await showRoomUseCase.typeExecute({hotel_id, room_type})
+
+    return response.status(200).json({rooms: rooms})
+  }
+  async read(request: Request, response: Response): Promise<Response> {
+    const {room_id} = request.params;
+
+    const showRoomUseCase = container.resolve(ShowRoomUseCase)
+
+    const room = await showRoomUseCase.readExecute(room_id)
 
 
+    return response.status(200).json({room: room})
+  }
+  async delete(request: Request, response: Response): Promise<Response> {
 
-    return response.status(201).json({rooms: []})
+      const {room_id} = request.params;
+
+      const deleteRoomUseCase = container.resolve(DeleteRoomUseCase)
+     const result = await deleteRoomUseCase.execute({room_id})
+
+    return response.status(200).json({result:result})
+  }
+  async update(request: Request, response: Response): Promise<Response> {
+    const { room_number, room_type, status, hotel_id , room_id} = request.body;
+
+      const updateRoomUseCase = container.resolve(UpdateRoomUseCase)
+    const rooms = await updateRoomUseCase.execute({
+      room_number,
+      room_type,
+      status,
+      hotel_id,
+      room_id
+    })
+
+
+    return response.status(201).json({rooms:rooms})
   }
 }
--- src/module/room/infra/http/routes/room.routes.ts
+++ src/module/room/infra/http/routes/room.routes.ts
@@ -1,18 +1,23 @@
 
 import { Request, Response, Router } from 'express';
-import { RoomControler } from '../controller/room.controller';
+
 import { validateSchema } from '@shared/middleware/validation.zod';
 import { CreateRoomDTO } from 'module/room/dtos/create-room-DTO';
+import { UpdateRoomDTO } from 'module/room/dtos/update-room-DTO';
+import { RoomController } from '../controller/room.controller';
 
 
-const usersRouter = Router();
-// Rotas
+const roomRouter = Router();
+// room
 
-const roomControler = new RoomControler()
+const roomController = new RoomController()
 
-usersRouter.post('/',validateSchema(CreateRoomDTO), roomControler.create );
+roomRouter.post('/',validateSchema(CreateRoomDTO), roomController.create ); // cria room
+roomRouter.get('/hotel/:hotel_id', roomController.show ); // mostrar todos os rooms de um determinado hotel
+roomRouter.get('/:room_id', roomController.read );// buscar rooom pelo id
+roomRouter.delete('/:room_id', roomController.delete ); // delete rom
+roomRouter.put('/', validateSchema(UpdateRoomDTO) ,roomController.update); // atualizar room
+roomRouter.get('/hotel/:hotel_id/:room_type',roomController.showType); // atualizar room
 
-usersRouter.get('/', roomControler.show );
 
-
-export default usersRouter;
+export default roomRouter;
