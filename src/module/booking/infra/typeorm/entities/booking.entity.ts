e8ff73c273ca39535848c5b51eca7f25c4ee9db6
Matheus Souza dos santos <msdsantos96@gmail.com>
2024-06-21 02:26:36 -0300

    create(booking): create booking entity

---
+++ src/module/booking/infra/typeorm/entities/booking.entity.ts
@@ -0,0 +1,54 @@
+
+import { MyBaseEntity } from '@shared/infra/typeorm/baseEntities/base.entities';
+import { Guest } from 'module/guest/infra/typeorm/entities/guest.entity';
+import { Hotel } from 'module/hotel/infra/typeorm/entities/hotel.entity';
+import { Room } from 'module/room/infra/typeorm/entities/room.entity';
+import {
+  Column,
+  Entity,
+  JoinColumn,
+  ManyToOne,
+  Timestamp,
+} from 'typeorm';
+
+@Entity('bookings')
+export class Booking extends MyBaseEntity {
+
+  @Column()
+  total_price: number;
+
+  @Column()
+  status: string;
+
+  @Column()
+  start_date_booking: Date;
+
+  @Column()
+  end_date_booking: Date;
+
+  @Column()
+  hotel_id: string;
+
+  @Column()
+  guest_id: string;
+
+  @Column()
+  room_id: string;
+
+  // relação
+  @ManyToOne(() => Hotel)
+  @JoinColumn({ name: 'hotel_id' })
+  hotel: Hotel;
+
+  @ManyToOne(() => Guest)
+  @JoinColumn({ name: 'guest_id' })
+  guest: Guest;
+
+  @ManyToOne(() => Room)
+  @JoinColumn({ name: 'room_id' })
+  room: Room;
+
+  constructor() {
+      super()
+  }
+}
