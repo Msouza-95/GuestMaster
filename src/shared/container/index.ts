74f5372fbde4d77e915b1da30d15bccd394a9df0
Matheus Souza dos santos <msdsantos96@gmail.com>
2024-06-21 04:56:31 -0300

    feat(container): inject

--- src/shared/container/index.ts
+++ src/shared/container/index.ts
@@ -1,4 +1,12 @@
 
+import BookingRepository from 'module/booking/infra/typeorm/repositories/booking.repository';
+import IBookingRepository from 'module/booking/repositories/I-booking-repository';
+import GuestRepository from 'module/guest/infra/typeorm/repositories/guest.repository';
+import IGuestRepository from 'module/guest/repositories/I-guest-repository';
+import HotelRepository from 'module/hotel/infra/typeorm/repositories/hotel.repository';
+import IHotelRepository from 'module/hotel/repositories/I-hotel-repository';
+import RoomRepository from 'module/room/infra/typeorm/repositories/room.repository';
+import IRoomRepository from 'module/room/repositories/I-room-repository';
 import UserRepository from 'module/user/infra/typeorm/repositories/user.repository';
 import IUserRepository from 'module/user/repositories/I-user-repository';
 import { container } from 'tsyringe';
@@ -7,4 +15,24 @@ import { container } from 'tsyringe';
 container.registerSingleton<IUserRepository>(
   'UserRepository',
   UserRepository
+);
+
+container.registerSingleton<IRoomRepository>(
+  'RoomRepository',
+  RoomRepository
+);
+
+container.registerSingleton<IGuestRepository>(
+  'GuestRepository',
+  GuestRepository
+);
+
+container.registerSingleton<IBookingRepository>(
+  'BookingRepository',
+  BookingRepository
+);
+
+container.registerSingleton<IHotelRepository>(
+  'HotelRepository',
+  HotelRepository
 );
