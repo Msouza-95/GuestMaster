bd76dcc31c236a423b60d139bb866dc82f336309
Matheus Souza dos santos <msdsantos96@gmail.com>
2024-06-21 20:51:13 -0300

    feat(guest): create update-guest

---
+++ src/module/guest/use-cases/update-guest.ts
@@ -0,0 +1,60 @@
+
+import { inject, injectable } from "tsyringe";;
+import AppError from "@shared/errors/AppError";
+import IGuestRepository from "../repositories/I-guest-repository";
+import { Guest } from "../infra/typeorm/entities/guest.entity";
+
+
+interface IRequest {
+    fullname : string;
+    email: string;
+    phone: string;
+    birthdayDate: Date;
+    city :string;
+    state: string;
+    country: string;
+    guest_id:string
+}
+
+@injectable()
+class UpdateGuestUseCase{
+  constructor(
+    @inject('GuestRepository')
+    private guestRepository: IGuestRepository,
+  ) {}
+
+   async execute({
+    fullname,
+    email,
+    phone,
+    birthdayDate,
+    city,
+    state,
+    guest_id
+  }:IRequest):Promise<Guest>{
+
+    /*
+     Verifica se hotel é valido
+    */
+    const guest = await this.guestRepository.findById(guest_id)
+    if(guest === null){
+      throw new AppError("The guest d'not exists", 404)
+
+    }
+
+    guest.fullname =fullname;
+    guest.email = email
+    guest.phone =phone;
+    guest.state =state;
+    guest.birthdayDate =birthdayDate;
+    guest.state =state;
+    guest.state =state
+
+    await this.guestRepository.saveGuest(guest)
+
+    return guest
+  }
+}
+
+
+export default UpdateGuestUseCase;
